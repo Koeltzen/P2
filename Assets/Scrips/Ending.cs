6ed65e9444c4cf81a088bc73facfb83f87dc0ea0
LyskjaerXD <KO32CG@student.aau.dk>
2026-05-05 14:00:40 +0200

    Ending3 test done forhåbentlig

--- Assets/Scrips/Ending.cs
+++ Assets/Scrips/Ending.cs
@@ -1,36 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndingManager : MonoBehaviour
+public class EndSceneLoader : MonoBehaviour
 {
-    private float GetTagCount(string tag)
+    public OutfitData data; // reference til dit script
+
+    public void LoadEndScene()
     {
-        GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
-        return objects.Length;
-    }
+        float points = data.playerTotalPoints;
 
-    public void LoadEnding()
-    {
-        float shoes = GetTagCount("Shoes");
-        float shirt = GetTagCount("Shirts");
-        float thickshirt = GetTagCount("ThickShirts");
-        float pants = GetTagCount("Pants");
-        float jacket = GetTagCount("Jacket");
-        float socks = GetTagCount("Socks");
-
-        float totalFeet = shoes + socks;
-        float totalUnder = pants;
-        float totalOver = shirt + thickshirt + jacket;
-
-        if (totalFeet < 12f || totalUnder < 12f || totalOver < 19f)
+        if (points < 21f)
         {
             SceneManager.LoadScene("Ending1");
         }
-        else if (
-                (totalFeet >= 12f && totalFeet <= 23f) ||
-                (totalUnder >= 12f && totalUnder <= 23f) ||
-                (totalOver >= 19f && totalOver <= 42f)
-        )
+        else if (points >= 21f && points <= 50f)
         {
             SceneManager.LoadScene("Ending2");
         }
